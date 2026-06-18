4ffb8c4596cd35a184c8be1f6528bb88033c9b35
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-18 17:50:14 +0700

    vv

---
+++ scene_main/Controller.java
@@ -0,0 +1,27 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Button goto_input;
+
+    @FXML
+    private Button goto_output;
+
+    @FXML
+    private Label label_result;
+
+    @FXML
+    void on_input(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_output(ActionEvent event) {
+
+    }
+
+}
