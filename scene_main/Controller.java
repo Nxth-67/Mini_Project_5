669c29d2184426f11fe70fcc564a1506405a9083
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-22 15:35:05 +0700

    xx

--- scene_main/Controller.java
+++ scene_main/Controller.java
@@ -25,7 +25,21 @@ public class Controller {
     private Label label_result;
 
     @FXML
-    void on_input(ActionEvent event) {
+    void on_input(ActionEvent event) throws IOException {
+
+        var stage = (Stage) label_result.getScene().getWindow();
+        
+        var view_input = getClass().getResource("/scene_input/View.fxml");
+        var controller_input = new scene_input.Controller();
+
+        var loader = new FXMLLoader();
+        loader.setLocation(view_input);
+        loader.setController(controller_input);
+
+        var scene = new Scene(loader.load());
+
+        stage.setScene(scene);
+        stage.show();
 
     }
 
@@ -36,11 +50,8 @@ public class Controller {
         var view_output = getClass().getResource("/scene_output/View.fxml");
         var controller_output = new scene_output.Controller();
 
-
-
         var loader = new FXMLLoader();
-        URL view_ouptut = null;
-        loader.setLocation(view_ouptut);
+        loader.setLocation(view_output);
         loader.setController(controller_output);
 
         var scene = new Scene(loader.load());
