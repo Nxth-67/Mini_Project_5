bf11e3bac74f6e837edf1ce57cf022c9ba3f33e9
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-22 14:01:53 +0700

    s

---
+++ scene_input/Controller.java
@@ -0,0 +1,23 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private Button button_save;
+
+    @FXML
+    private Label label_input;
+
+    @FXML
+    private TextField textfield;
+
+    @FXML
+    void on_save(ActionEvent event) {
+
+    }
+
+}
