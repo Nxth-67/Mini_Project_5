a935a6e80a838402d70bad31df2f383f076ddec1
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-22 14:56:48 +0700

    ss

---
+++ global/Global.java
@@ -0,0 +1,19 @@
+package global;
+
+public class Global {
+    
+    private String data;
+
+    // setter
+    void set_data(String data) {
+        // business logic
+        this.data = data;
+    }
+
+    // getter
+    String get_data() {
+        // business logic
+        return this.data;
+    }
+
+}
